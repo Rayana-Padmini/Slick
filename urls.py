9406ffae371058947eac669a37730dc0ca9e50da
Rayana-Padmini <prayana65@gmail.com>
2017-07-03 19:45:28 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,12 +1,32 @@
+"""locationSlick URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+import django
 from django.conf.urls import url
 from django.contrib import admin
-import views
+from django.conf import settings
+from django.conf.urls import include
+import debug_toolbar
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^location/',include('slickApp.urls')),
+    url(r'^auth/',include('django.contrib.auth.urls')),
+    #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
-app_name="slickApp"
-
-urlpatterns=[
-    url(r'test1',views.test1),
-    url(r'temples/([0-9])$',views.temples,name="temples"),
-    url(r'temples/rating/$',views.templerating,name="location"),
-    url(r'reg/$', views.reg_form)
 ]
+
+urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
